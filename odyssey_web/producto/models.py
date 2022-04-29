3281f1e13969ab04d3e4b8684a4af25c8a2fef69
madv1ll <kevint.mtn@gmail.com>
2022-04-29 09:09:12 -0400

    modificacion tabla producto

--- odyssey_web/producto/models.py
+++ odyssey_web/producto/models.py
@@ -1,31 +1,6 @@
 from django.db import models
 from web.models import Pais, Region, Comuna
 
-# class Pais(models.Model):
-#     id_pais = models.AutoField(primary_key=True)
-#     sigla   = models.CharField(max_length=3)
-#     nombre  = models.CharField(max_length=60,null=False)
-
-#     def __str__(self):
-#         return self.nombre
-
-# class Region(models.Model):
-#     id_region = models.AutoField(primary_key=True)
-#     nombre    = models.CharField(max_length=60)
-#     id_pais   = models.ForeignKey(Pais, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.nombre
-
-# class Comuna(models.Model):
-#     id_comuna = models.AutoField(primary_key=True)
-#     sigla     = models.CharField(max_length=3)
-#     nombre    = models.CharField(max_length=30)
-#     id_region   = models.ForeignKey(Region, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.nombre
-
 class Proveedor(models.Model):
     id_proveedor = models.AutoField(primary_key=True)
     rut          = models.CharField(max_length=12, null=True) 
@@ -46,8 +21,8 @@ class Categoria(models.Model):
 
 class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50) # AGREGAR AL MODELO!!!!!!!!!!
-    descripcion = models.TextField()
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=70)
     imagen = models.ImageField(upload_to="productos", null=True)
     precio = models.IntegerField()
     id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
