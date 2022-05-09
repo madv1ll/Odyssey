ee23064d89be84582a09598aaf6814cf3d8eec4d
Kevin <kevint.mtn@gmail.com>
2022-05-08 22:01:04 -0400

    avance creacion detalle compra

--- odyssey_web/carrito/models.py
+++ odyssey_web/carrito/models.py
@@ -2,30 +2,16 @@ import decimal
 from django.db import models
 from user.models import Usuario
 from producto.models import Producto
-class Carrito(models.Model):
-    carro_id = models.CharField(max_length=100, null = False, blank=False, unique=True)
-    rut_cliente =  models.ForeignKey(Usuario, null=True, blank=True, on_delete=models.CASCADE)
+import uuid
+from django.db.models.signals import pre_save
+from django.db.models.signals import m2m_changed
+class Compra(models.Model):
+    id_compra = models.AutoField(primary_key=True)
+    rut_usuario =  models.ForeignKey(Usuario, on_delete=models.CASCADE)
     producto = models.ManyToManyField(Producto)
     subtotal = models.DecimalField(default=0, max_digits = 8, decimal_places=0)
     total = models.DecimalField(default=0, max_digits = 8, decimal_places=0)
     fecha = models.DateTimeField(auto_now_add=True)
-
-    IVA = 0.19 #comision IVA
-
-    def __str__(self):
-        return self.carro_id
-
-    def update_totals(self):
-        self.update_subtotal()
-        self.update_total()
-
-    def update_subtotal(self):
-        self.subtotal = sum([ producto.precio  for producto in self.products.all()]) 
-        self.save()  
-
-    def update_total(self):
-        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Carrito.FEE))
-        self.save()
 
 class Detalle_compra(models.Model):
     id_detalle = models.AutoField(primary_key=True)
--- odyssey_web/carrito/views.py
+++ odyssey_web/carrito/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .carro import Carro
 from producto.models import Producto
 from user.models import Usuario
-from .models import Carrito, Detalle_compra
+from .models import  Detalle_compra
 from django.shortcuts import  redirect
 from django.views.generic.edit import CreateView
 from django.views.generic import View
@@ -53,7 +53,6 @@ class CarritoView(View):
         total = 0
         for key, value in request.session["carro"].items():
             total=total+(float(value["precio"]))
-        print(request.session["carro"].items())
         amount = total
         buy_order="1"
         session_id="1"
@@ -75,20 +74,29 @@ class DetalleCompra(View):
         # print(success)
         if success.get("status") == "AUTHORIZED":
             if request.user.is_active:
-                request.user.rut
-            # aqui se creara el registro q se enviara a la BD el modelo se llama Carrito
-            # registro = {
-            #         rut_cliente =  
-            #         producto = models.ManyToManyField(Producto)
-            #         subtotal = models.DecimalField(default=0, max_digits = 8, decimal_places=0)
-            #         total = models.DecimalField(default=0, max_digits = 8, decimal_places=0)
-            #         fecha = models.DateTimeField(auto_now_add=True)
-            # }
+                cliente = request.user.rut
+            else:
+                cliente = "99.999.999-K"
+            total = 0
+            obj_cliente = Usuario.objects.only('rut').get(rut=cliente)
+            for key, value in request.session["carro"].items():
+                total=total+(float(value["precio"]))
+                # creacion del registro del producto
+                registro = ""
+                registro = Detalle_compra(
+                    id_producto     = value["producto_id"],
+                    rut_cliente     = obj_cliente,
+                    precio_producto = value["precio"],
+                    cantidad        = value["cantidad"]
+                )
+                registro.save()
+
             Carro.limpiar_carro(request)
         else:
             # print("rechazado")
             registro = "no crear registro"
         return render(request,"carro/confirmacion.html",{"success":success})
+
 	# DEBITO = 4051 8842 3993 7763
     # 11 111 111 1
     # 123
@@ -105,16 +113,3 @@ class DetalleCompra(View):
 #  'payment_type_code': 'VD', 
 #  'response_code': 0, 
 #  'installments_number': 0}
-
-# ## detalle de compra
-# class DetalleCompra_Crear(CreateView): 
-#     model = Detalle_compra # Llamamos a la clase 'Postres' que se encuentra en nuestro archivo 'models.py'
-#     form = Detalle_compra # Definimos nuestro formulario con el nombre de la clase o modelo 'Postres'
-#     fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'postres' de nuestra Base de Datos 
-#     # Redireccionamos a la página principal luego de crear un registro o postre
-#     def get_success_url(self, request):        
-#         return render(request, 'web/agregar_proveedor.html')
-
-
-# def detalle_compra(request):
-#     return render('')
