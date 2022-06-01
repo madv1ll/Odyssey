2b146dc46cad2b19b8c9a84b2b89412e2cc8cf63
Kevin <kevint.mtn@gmail.com>
2022-06-01 17:11:54 -0400

    feat: disminucion de stock

--- SQL.sql
+++ SQL.sql
@@ -399,3 +399,23 @@ INSERT INTO CARRITO_TIPO_PAGO VALUES ('SI', '3 cuotas sin interes');
 INSERT INTO CARRITO_TIPO_PAGO VALUES ('S2', '2 cuotas sin interes');
 INSERT INTO CARRITO_TIPO_PAGO VALUES ('NC', 'N Cuotas sin interes');
 INSERT INTO CARRITO_TIPO_PAGO VALUES ('VP', 'Venta Prepago');
+
+/* PROCEDIMIENTO ALMACENADO QUE DISMINUYE STOCK */
+    CREATE OR REPLACE PROCEDURE SP_STOCK_PROD
+    (p_id_prod NUMBER, p_cant NUMBER) IS
+        cant_actual NUMBER(8); 
+    BEGIN
+        SELECT stock INTO cant_actual FROM producto_producto WHERE id_producto = p_id_prod;
+        UPDATE producto_producto 
+            SET stock = (cant_actual - p_cant) 
+        WHERE id_producto = p_id_prod ;
+    END;
+
+/* TRIGGER QUE LLAMA AL PROCEDIMIENTO ANTERIOR*/
+
+    CREATE OR REPLACE TRIGGER TRG_DISMINUYE_STOCK
+    AFTER INSERT ON carrito_detalle_compra
+    FOR EACH ROW
+    BEGIN
+        SP_STOCK_PROD(:NEW.id_producto_id,:NEW.cantidad);
+    END;
