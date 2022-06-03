af2d2dc41ef586dbdcab968a5dd5059b29aa65cb
madv1ll <kevint.mtn@gmail.com>
2022-06-03 15:54:43 -0400

    se agregan imagenes productos

--- odyssey_web/odyssey_web/settings.py
+++ odyssey_web/odyssey_web/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'odyssey_web.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.oracle',
-    #     'NAME': 'xe',
-    #     'USER': 'ODYSSEY_BD',
-    #     'PASSWORD': 'oraodypass22',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # },
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'ODYSSEY_BD',
+        'PASSWORD': 'oraodypass22',
+    },
 }
 
 
