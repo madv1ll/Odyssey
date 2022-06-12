0c5f5280d7a816b0eb007ad9af72e87baf1b5155
fabian ignacio salazar sepulveda <energyfabian12@gmail.com>
2022-06-11 22:42:45 -0400

    proteccion rutas gestion de usuarios

--- odyssey_web/odyssey_web/settings.py
+++ odyssey_web/odyssey_web/settings.py
@@ -150,7 +150,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST    = "smtp.office365.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT    = 587
-# EMAIL_HOST_USER = "odyssseygamming@gmail.com"
 EMAIL_HOST_USER = "odysseygamming@outlook.com"
 EMAIL_HOST_PASSWORD = "portafolio123"
 SERVER_EMAIL = EMAIL_HOST_USER
