6f0c46bfe25c78c4b2464b603a4083df6701b525
fabian ignacio salazar sepulveda <energyfabian12@gmail.com>
2022-06-14 21:45:39 -0400

    vista de detalles de productos y su template

--- odyssey_web/web/urls.py
+++ odyssey_web/web/urls.py
@@ -1,14 +1,16 @@
 from re import template
 from django.urls import path
 from .views import home, listar_productosVenta, nosotros, agregar_presentacion1, modificar_presentacion1, agregar_presentacion2, \
-    modificar_presentacion2, agregar_presentacion3, modificar_presentacion3
+    modificar_presentacion2, agregar_presentacion3, modificar_presentacion3, detalleProducto
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('nosotros/', nosotros, name="nosotros"),
+    #productos
     path('productos/', listar_productosVenta, name="productosVenta"),
-
+    path('productos/', listar_productosVenta, name="productosVenta"),
+    path('detalle_producto/<id>/',detalleProducto, name="detalleProducto"),
     #presentaciones en pestaña nosotros
     path('agregar-presentacion1/', agregar_presentacion1, name="agregar_presentacion1"),
     path('modificar-presentacion1/<id>/', modificar_presentacion1, name="modificar_presentacion1"),
